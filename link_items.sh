b36182cb4a55dc92f69a1897ebf1b868df398e32
FolkerD0C <104650641+FolkerD0C@users.noreply.github.com>
2024-01-02 22:02:17 +0100

    Fix defectious formatting

--- link_items.sh
+++ link_items.sh
@@ -10,6 +10,6 @@ done
 
 FILES_TO_SET="$(find -type f | cut -c3- | grep -v '^\.git/' | grep -v '\.sh$')"
 
-for file_path in "${FILES_TO_SET[@]}"; do
+for file_path in ${FILES_TO_SET[@]}; do
     ln -s -f "$DOTFILES_DIR/$file_path" "$HOME/$file_path"
 done
