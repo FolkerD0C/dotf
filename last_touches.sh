0afbb9ea598806fb7cb4e1d31bf9d1f073ccb53f
FolkerD0C <104650641+FolkerD0C@users.noreply.github.com>
2024-01-08 18:11:39 +0100

    Change .zshrc.priv creation

--- last_touches.sh
+++ last_touches.sh
@@ -3,18 +3,21 @@
 ZSHRC_PRIV_PATH="${HOME}/.zshrc.priv"
 
 if [ ! -f "$ZSHRC_PRIV_PATH" ]; then
-  echo "Creating new file: $ZSHRC_PRIV_PATH"
-  echo 'It is a file which is sourced by ~/.zshrc'
-  echo 'and it is not included in the dotfiles repository.'
-  cat <<EOF > $ZSHRC_PRIV_PATH
+  echo "   Creating new file: $ZSHRC_PRIV_PATH"
+  echo '   It is a file which is sourced by ~/.zshrc'
+  echo '   and it is not included in the dotfiles repository.'
+  cat <<EOF > "$ZSHRC_PRIV_PATH"
 # This is an autocreated file by the installer scripts of 
 #      https://github.com/FolkerD0C/dotfiles
 # This file is not part of the repository but is sourced by its .zshrc
 # You can place private statements (eg. aliases and environmental variables)
 # or functions here.
+#
+# Uncomment the following line to have lsp capabilities in neovim.
+# Currently only bash and lua are supported
+# export NEOVIM_REQUIRED_LANGUAGES='bash,lua'
+#
+# Uncomment the following line to have cargo/rust enabled in your shell
+# source "${HOME}/.cargo/env"
 EOF
-fi
-
-if grep -q '^export NEOVIM_REQUIRED_LANGUAGES=' "$ZSHRC_PRIV_PATH"; then
-  echo 'export NEOVIM_REQUIRED_LANGUAGES=lua' >> "$ZSHRC_PRIV_PATH"
 fi
